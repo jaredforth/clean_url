52f8e948a7a305f37a9825c6ddcc5db0e6d5c620
Jared Forth <jaredforthdev@gmail.com>
2020-09-29 14:52:04 -0500

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 use url::{Url, ParseError};
+use reqwest::blocking::Client;
 
 /// Returns a URL
+pub fn return_url(url: String) -> Option<String> {
+    parse_url(url)
+}
+
+
+/// This function parses an incomplete URL
+/// and returns the full URL.
 ///
 /// ## Usage:
 ///
@@ -10,11 +18,7 @@ use url::{Url, ParseError};
 /// assert_eq!(Some(String::from("http://example.com/")), return_url(String::from("example.com")));
 /// assert_eq!(Some(String::from("http://www.example.com/")), return_url(String::from("www.example.com")));
 /// ```
-pub fn return_url(url: String) -> Option<String> {
-    parse_url(url)
-}
-
-fn parse_url(url: String) -> Option<String> {
+pub fn parse_url(url: String) -> Option<String> {
     match Url::parse(url.as_str()) {
         Ok(u) => {
             println!("{:?}", u);
@@ -28,6 +32,41 @@ fn parse_url(url: String) -> Option<String> {
                 }
                 _ => { None }
             }
+        }
+    }
+}
+
+/// This function checks the status code
+/// of a URL and returns the version of the
+/// URL that is a 200 series.
+///
+/// ## Usage:
+///
+/// ```
+/// use servstat::check_status;
+///
+/// assert_eq!(check_status(String::from("https://httpbin.org/status/200")), Some(String::from("https://httpbin.org/status/200")));
+/// assert_eq!(check_status(String::from("http://www.bertsmithco.com")), Some(String::from("https://bertsmithco.com/")));
+/// assert_eq!(check_status(String::from("https://www.bertsmithco.com")), Some(String::from("https://bertsmithco.com/")));
+/// ```
+pub fn check_status(url: String) -> Option<String> {
+    let client = Client::new();
+    match client.get(url.as_str()).send() {
+        Ok(r) => {
+            let code = r.status();
+            let resp_url = r.url();
+            println!("{:?} - {:?}", code, resp_url);
+            if code.is_success() {
+                Some(resp_url.to_string())
+            } else {
+                // TODO Does the URL use www? If so, remove it
+                // TODO Does the URL not use www? If so, add it
+                None
+            }
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            None
         }
     }
 }
