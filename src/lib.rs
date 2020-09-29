079dc4b8821f75d0d4ba80383d55903ff75174f3
Jared Forth <jaredforthdev@gmail.com>
2020-09-29 14:13:19 -0500

    pass first test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,32 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use url::{Url, ParseError};
+
+/// Returns a URL
+///
+/// ## Usage:
+///
+/// ```
+/// use servstat::return_url;
+///
+/// assert_eq!(String::from("http://example.com/"), return_url(String::from("example.com")));
+/// ```
+pub fn return_url(url: String) -> String {
+    parse_url(url)
+}
+
+fn parse_url(url: String) -> String {
+    match Url::parse(url.as_str()) {
+        Ok(u) => {
+            println!("{:?}", u);
+            u.into_string()
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            match e {
+                ParseError::RelativeUrlWithoutBase => {
+                    parse_url(format!("http://{}", url))
+                }
+                _ => { String::from("match error") }
+            }
+        }
     }
 }
